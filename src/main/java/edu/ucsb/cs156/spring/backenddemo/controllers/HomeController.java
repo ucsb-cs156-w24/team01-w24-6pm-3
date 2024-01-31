345cec6c2e753dd7ce2907057d0ac2df8c603b33
Jennifer Lopez <jenniferlopez@169-231-203-131.wireless.ucsb.edu>
2024-01-30 17:21:13 -0800

    jl - updated home controller

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeController.java
@@ -43,12 +43,13 @@ public class HomeController {
         resultMap.put("greeting", "Greetings from Spring Boot!");
 
         List<String> team = new ArrayList<String>();
-        team.add("Jing P.");
-        team.add("Christian S.");
-        team.add("Guy W.");
-        team.add("Phill C.");
+        team.add("Jennifer L.");
+        team.add("Kenisha V.");
+        team.add("Andy J.");
+        team.add("Towela P.");
+        team.add("Jennie W.");
         resultMap.put("team", team);
-        resultMap.put("repo", "https://github.com/ucsb-cs156-w24/STARTER-team01");
+        resultMap.put("repo", "https://github.com/ucsb-cs156-w24/team01-w24-6pm-3");
         resultMap.put("api-documentation", baseUrl + "swagger-ui/index.html");
         return mapper.writeValueAsString(resultMap);
     }
